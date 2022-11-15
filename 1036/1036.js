02a9a2a1d31b334b31b92bf71481b32c23890e42
DenisScar <denis.scar@gmail.com>
2022-11-15 13:51:01 -0300

    modified 1036

--- 1036/1036.js
+++ 1036/1036.js
@@ -5,3 +5,14 @@ let a = parseFloat(lines.shift());
 let b = parseFloat(lines.shift());
 let c = parseFloat(lines.shift());
 
+let delta = Math.pow(b,2) - 4 * a * c;
+
+if(delta < 0 || a === 0){
+    console.log("Impossivel calcular");
+}else{
+    let r1 = (-b + Math.sqrt(delta)) / (2 * a);
+    let r2 = (-b - Math.sqrt(delta)) / (2 * a);
+
+    console.log(`R1 = ${r1.toFixed(5)}`);
+    console.log(`R2 = ${r2.toFixed(5)}`);
+}
